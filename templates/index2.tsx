9140ec3a0a763126e5e26d41941e238b88d6afb1
Evelyn França <60625481+evelynsfranca@users.noreply.github.com>
2024-04-26 14:32:43 -0300

    Create index2.tsx

---
+++ templates/index2.tsx
@@ -0,0 +1,12 @@
+export default function Home2() {
+  return (
+    <main>
+      <div>
+        <p>Home</p>
+        <div>
+          <a href="/"><%= applicationName %></a>
+        </div>
+      </div>
+    </main>
+  );
+}
